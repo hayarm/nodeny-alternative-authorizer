00fb9b60fba0145f41e5ca671c99cf54384c9d79
Ramzesich <Ramzesich@22cadbc0-10ad-11df-95c3-6ff5f211243c>
2010-02-03 10:27:57 +0000

    Initial code commit

---
+++ trunk/Authorizer/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Authorizer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.labelPasswd = new System.Windows.Forms.Label();
+            this.textBoxPasswd = new System.Windows.Forms.TextBox();
+            this.buttonAuth = new System.Windows.Forms.Button();
+            this.checkBoxSave = new System.Windows.Forms.CheckBox();
+            this.checkBoxAutostart = new System.Windows.Forms.CheckBox();
+            this.notifyIconAuth = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.linkPing = new System.Windows.Forms.LinkLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(79, 38);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(182, 20);
+            this.textBoxUser.TabIndex = 0;
+            this.textBoxUser.TextChanged += new System.EventHandler(this.doTriggerButton);
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(5, 41);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(41, 13);
+            this.labelUser.TabIndex = 1;
+            this.labelUser.Text = "Логин:";
+            // 
+            // labelPasswd
+            // 
+            this.labelPasswd.AutoSize = true;
+            this.labelPasswd.Location = new System.Drawing.Point(5, 67);
+            this.labelPasswd.Name = "labelPasswd";
+            this.labelPasswd.Size = new System.Drawing.Size(48, 13);
+            this.labelPasswd.TabIndex = 3;
+            this.labelPasswd.Text = "Пароль:";
+            // 
+            // textBoxPasswd
+            // 
+            this.textBoxPasswd.Location = new System.Drawing.Point(79, 64);
+            this.textBoxPasswd.Name = "textBoxPasswd";
+            this.textBoxPasswd.PasswordChar = '*';
+            this.textBoxPasswd.Size = new System.Drawing.Size(182, 20);
+            this.textBoxPasswd.TabIndex = 2;
+            this.textBoxPasswd.TextChanged += new System.EventHandler(this.doTriggerButton);
+            // 
+            // buttonAuth
+            // 
+            this.buttonAuth.Location = new System.Drawing.Point(8, 141);
+            this.buttonAuth.Name = "buttonAuth";
+            this.buttonAuth.Size = new System.Drawing.Size(253, 23);
+            this.buttonAuth.TabIndex = 4;
+            this.buttonAuth.Text = "Включить Интернет";
+            this.buttonAuth.UseVisualStyleBackColor = true;
+            this.buttonAuth.Click += new System.EventHandler(this.buttonAuth_Click);
+            // 
+            // checkBoxSave
+            // 
+            this.checkBoxSave.AutoSize = true;
+            this.checkBoxSave.Location = new System.Drawing.Point(8, 95);
+            this.checkBoxSave.Name = "checkBoxSave";
+            this.checkBoxSave.Size = new System.Drawing.Size(162, 17);
+            this.checkBoxSave.TabIndex = 6;
+            this.checkBoxSave.Text = "Запомнить логин и пароль";
+            this.checkBoxSave.UseVisualStyleBackColor = true;
+            this.checkBoxSave.CheckedChanged += new System.EventHandler(this.checkBoxSave_CheckedChanged);
+            // 
+            // checkBoxAutostart
+            // 
+            this.checkBoxAutostart.AutoSize = true;
+            this.checkBoxAutostart.Location = new System.Drawing.Point(8, 118);
+            this.checkBoxAutostart.Name = "checkBoxAutostart";
+            this.checkBoxAutostart.Size = new System.Drawing.Size(197, 17);
+            this.checkBoxAutostart.TabIndex = 7;
+            this.checkBoxAutostart.Text = "Запускаться при старте системы";
+            this.checkBoxAutostart.UseVisualStyleBackColor = true;
+            this.checkBoxAutostart.CheckedChanged += new System.EventHandler(this.checkBoxAutostart_CheckedChanged);
+            // 
+            // notifyIconAuth
+            // 
+            this.notifyIconAuth.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconAuth.Icon")));
+            this.notifyIconAuth.Text = "Линет - отключено";
+            this.notifyIconAuth.Visible = true;
+            this.notifyIconAuth.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Icon_onClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 175);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(272, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(101, 17);
+            this.toolStripStatusLabel.Text = "Нет соединения";
+            // 
+            // linkPing
+            // 
+            this.linkPing.AutoSize = true;
+            this.linkPing.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkPing.Location = new System.Drawing.Point(230, 180);
+            this.linkPing.Name = "linkPing";
+            this.linkPing.Size = new System.Drawing.Size(36, 13);
+            this.linkPing.TabIndex = 9;
+            this.linkPing.TabStop = true;
+            this.linkPing.Text = "Пинг";
+            this.linkPing.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkPing_LinkClicked);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem,
+            this.aboutMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(272, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(148, 20);
+            this.ToolStripMenuItem.Text = "Сменить адрес сервера";
+            this.ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // aboutMenuItem
+            // 
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.aboutMenuItem.Text = "О программе";
+            this.aboutMenuItem.Click += new System.EventHandler(this.aboutMenuItem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 197);
+            this.Controls.Add(this.linkPing);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.checkBoxAutostart);
+            this.Controls.Add(this.checkBoxSave);
+            this.Controls.Add(this.buttonAuth);
+            this.Controls.Add(this.labelPasswd);
+            this.Controls.Add(this.textBoxPasswd);
+            this.Controls.Add(this.labelUser);
+            this.Controls.Add(this.textBoxUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизатор";
+            this.Load += new System.EventHandler(this.Window_onLoad);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Window_onClosing);
+            this.Resize += new System.EventHandler(this.Window_onResize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label labelPasswd;
+        private System.Windows.Forms.TextBox textBoxPasswd;
+        private System.Windows.Forms.Button buttonAuth;
+        private System.Windows.Forms.CheckBox checkBoxSave;
+        private System.Windows.Forms.CheckBox checkBoxAutostart;
+        private System.Windows.Forms.NotifyIcon notifyIconAuth;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.LinkLabel linkPing;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenuItem;
+    }
+}
+
---
+++ trunk/Authorizer/Form2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Authorizer
+{
+    public partial class DomainWindow : Form
+    {
+        public DomainWindow(string domain)
+        {
+            InitializeComponent();
+            textBoxDomain.Text = domain;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Registry.LocalMachine.DeleteSubKey(@"SOFTWARE\AuthorizerDomain");
+            }
+            catch
+            {
+                // don't want to delete? I can live with that
+            }
+            RegistryKey domainKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\AuthorizerDomain");
+            if (domainKey != null)
+            {
+                try
+                {
+                    domainKey.SetValue("domain", textBoxDomain.Text);
+                }
+                catch (Exception ex)
+                {
+                    return;
+                }
+                finally
+                {
+                    domainKey.Close();
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
